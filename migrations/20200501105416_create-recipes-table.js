713444658625cd4c3aaecb67ce7c7ab5a6413b25
DarneaPinkett <56055954+DarneaPinkett@users.noreply.github.com>
2020-05-01 11:27:47 -0400

    migrations

---
+++ migrations/20200501105416_create-recipes-table.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('recipes', tbl => {
+      tbl.increments();
+      tbl.string("Name", 128)
+      .unique()
+      .notNullable();
+  })
+  .createTable("steps", tbl => {
+      tbl.increments();
+      tbl.integer('step_number')
+      .notNullable();
+      tbl.text('instructions')
+      .notNullable();
+      tbl.integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipes')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  })
+  .createTable("ingredients", tbl => {
+      tbl.increments();
+      tbl.string("Name", 128)
+      .notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists("ingredients")
+  .dropTableIfExists("steps")
+  .dropTableIfExists("recipes");
+};
